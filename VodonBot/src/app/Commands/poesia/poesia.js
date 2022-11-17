1bb5ada750b83bada934a90a5acdbba973aadf49
tiantianana <tiantian.ana@gmail.com>
2022-11-17 02:56:21 +0100

    2.0

--- VodonBot/src/app/Commands/poesia/poesia.js
+++ VodonBot/src/app/Commands/poesia/poesia.js
@@ -1,15 +1,34 @@
+let isUpdating = {}
 module.exports = {
     name: "poesia",
     aliases: ["poesia"],
     description: "Generar aleatoriamente una poesia",
     run: async (client, message, args) => {
+
+        message.channel.send('Hola')
+
+    
+        let palabras = []
+        let exit = false
+        isUpdating[message.guild.id] = true;
+
+        while (!exit) {
+
+            answer = await message.channel.awaitMessages((m => m.author.id === message.author.id), {
+                max: 1,
+                time: 120000,
+                errors: ['time']
+            }).catch(function () {
+                delete isUpdating[question.guild.id]
+                question.delete({timeout: 1000})
+                message.channel.send(emojis.clock + " Time's up! Please run the Commands again!").then(m => m.delete({timeout: 10000}))
+            });
+
+            console.log(answer)
+
+        }
+
         
-        let embed = new Discord.MessageEmbed() //Creamos otro embed
-                .setColor('black')
-                .setTimestamp()
-                .setTitle("Poesia")
-                .setDescription('Aqui Poesia')
-        message.channel.send(embed)
 
     }
 }
